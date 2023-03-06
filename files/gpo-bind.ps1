fd177ec2c6decf59f5be3c8b2a302238991f915e
Sven Jansen <sven.jansen@devicetrust.com>
2023-03-06 14:39:43 +0100

    .

--- files/gpo-bind.ps1
+++ files/gpo-bind.ps1
@@ -12,6 +12,6 @@ $target = "DC=training,DC=dt,DC=democloud"
 $GPOs = "GPO_C_IPv6","GPO_C_PowerSettings","GPO_U_Optics_Wallpaper"
 link-gpos -target $target -GPOs $GPOs
 
-$target = "OU=Computer,OU=Objects,DC=training,DC=dt,DC=democloud"
+$target = "OU=Computer,OU=Objects,DC=dt,DC=democloud"
 $GPOs = "GPO_C_DisableServerManagerOnStartup","GPO_C_LoopbackProcessing","GPO_C_NewsAndInterests","GPO_C_deviceTRUST_Geolocation_Google","GPO_C_RegionSettings","GPO_C_deviceTRUST_Basic","GPO_C_deviceTRUST_Demo_Demobox"
 link-gpos -target $target -GPOs $GPOs
