d5d4d18c226a5eaa473ae0decd56d78cf53a574f
Sven Jansen <sven.jansen@devicetrust.com>
2023-03-06 14:38:21 +0100

    .

--- files/gpo-bind.ps1
+++ files/gpo-bind.ps1
@@ -9,9 +9,9 @@ function link-gpos {
 }
 
 $target = "DC=training,DC=dt,DC=democloud"
-$GPOs = "GPO_C_Disable_LLMNR","GPO_C_IPv6","GPO_C_PowerSettings","GPO_U_Optics_Wallpaper"
+$GPOs = "GPO_C_IPv6","GPO_C_PowerSettings","GPO_U_Optics_Wallpaper"
 link-gpos -target $target -GPOs $GPOs
 
 $target = "OU=Computer,OU=Objects,DC=training,DC=dt,DC=democloud"
-$GPOs = "GPO_C_DisableServerManagerOnStartup","GPO_C_LoopbackProcessing","GPO_C_NewsAndInterests","GPO_C_RDSH","GPO_C_deviceTRUST_Basic","GPO_C_deviceTRUST_Geolocation_Google"
+$GPOs = "GPO_C_DisableServerManagerOnStartup","GPO_C_LoopbackProcessing","GPO_C_NewsAndInterests","GPO_C_deviceTRUST_Geolocation_Google","GPO_C_RegionSettings","GPO_C_deviceTRUST_Basic","GPO_C_deviceTRUST_Demo_Demobox"
 link-gpos -target $target -GPOs $GPOs
